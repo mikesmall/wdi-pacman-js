4b2a714a73881ad5617198c64c823a5b7f31b1c1
mikesmall <michael.l.a.small@gmail.com>
2017-06-30 14:58:04 -0400

    'Eat Ghost' options in menu, temporarily set for  eatDot function

--- pacman.js
+++ pacman.js
@@ -61,6 +61,10 @@ function displayStats() {
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
+  console.log('(1) Eat Inky');
+  console.log('(2) Eat Blinky');
+  console.log('(3) Eat Pinky');
+  console.log('(4) Eat Clyde');
   console.log('(q) Quit');
 }
 
@@ -85,6 +89,18 @@ function processInput(key) {
       process.exit();
       break;
     case 'd':
+      eatDot();
+      break;
+    case '1':
+      eatDot();
+      break;
+    case '2':
+      eatDot();
+      break;
+    case '3':
+      eatDot();
+      break;
+    case '4':
       eatDot();
       break;
     default:
